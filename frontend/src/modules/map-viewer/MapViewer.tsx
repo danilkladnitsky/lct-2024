5139b45d50e3b03d23eafee8db6300dbc8f00fea
= <=>
2024-06-15 20:30:23 +0800

    feat: add map gl

--- frontend/src/modules/map-viewer/MapViewer.tsx
+++ frontend/src/modules/map-viewer/MapViewer.tsx
@@ -1,6 +1,7 @@
 import { Card } from "@gravity-ui/uikit";
 import classNames from "classnames";
 
+import { Map } from "react-map-gl";
 import styles from "./MapViewer.module.scss";
 
 interface Props {
@@ -9,6 +10,15 @@ interface Props {
 
 export const MapViewer = ({ className }: Props) => {
   return (
-    <Card className={classNames(styles.mapViewer, className)}>MapViewer</Card>
+    <Card className={classNames(styles.mapViewer, className)}>
+      <Map
+        initialViewState={{
+          longitude: -91.874,
+          latitude: 42.76,
+          zoom: 12,
+        }}
+        mapStyle="mapbox://styles/mapbox/satellite-v9"
+      ></Map>
+    </Card>
   );
 };
