9c547d150e0ae18cf0febde877f06edf7a7b8240
Danil Kladnitsky <danyakladnitsky@gmail.com>
2024-06-16 01:39:40 +0800

    fix: trigger backend

--- backend/app.py
+++ backend/app.py
@@ -23,7 +23,7 @@ def get_object():
     return jsonify({"message": "JSON received", "data": shool.objects}), 200
 
 
-@app.route('/view/render')
+@app.route('/render')
 def index():
     with open('example_request.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
